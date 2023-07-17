bf821ef64fe5611d36858e4eca953eedaf372034
Sid <sidbose93@gmail.com>
2023-07-17 02:35:27 -0500

    Changed images and slogan.

--- script/script.js
+++ script/script.js
@@ -1,15 +1,7 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
+header.classList.add('navbarDark');
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
